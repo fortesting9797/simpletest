39497ccb1316f0e7112a5bca4926824d16c4fa1f
fortesting9797 <fortesting9797@gmail.com>
2025-02-18 16:17:16 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+#Simple React Component
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, GitHub Actions! 🚀</h1>
+      <p>This is a simple React app deployed using GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
